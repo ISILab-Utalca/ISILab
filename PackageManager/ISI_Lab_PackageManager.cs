a0e3eb1269e74804fb6553620487aaea33bc706e
SebastianFranMartin <65563860+SebastianFranMartin@users.noreply.github.com>
2025-11-03 15:00:40 -0300

    Update ISI_Lab_PackageManager.cs

--- PackageManager/ISI_Lab_PackageManager.cs
+++ PackageManager/ISI_Lab_PackageManager.cs
@@ -8,19 +8,42 @@ using System.Collections.Generic;
 using ISILab.LBS.Settings;
 using ISILab.LBS.Internal;
 
+[InitializeOnLoad]
 public class ISI_Lab_PackageManager : IPackageManagerExtension
 {
     const string defaultSettingsGUID = "29abd09f3cff7644da7097258d0ae978";
     const string defaultStorageGUID = "5dacd13b749bccf469893489a5d0f94b";
 
-    public VisualElement CreateExtensionUI()
+    static ISI_Lab_PackageManager()
     {
-        return null;
+        PackageManagerExtensions.RegisterExtension(new ISI_Lab_PackageManager());
     }
 
     public void OnPackageAddedOrUpdated(UnityEditor.PackageManager.PackageInfo packageInfo)
     {
         Debug.Log("ON PACKAGE ADDED OR UPDATED");
+
+        switch (packageInfo.name)
+        {
+            case "com.isilab.lbs":
+                InitializeLBS(packageInfo);
+                break;
+
+            default:
+                // A different package was modified.
+                break;
+        }
+    }
+
+    public void OnPackageRemoved(UnityEditor.PackageManager.PackageInfo packageInfo) { }
+
+    public void OnPackageSelectionChange(UnityEditor.PackageManager.PackageInfo packageInfo) { }
+
+    public VisualElement CreateExtensionUI() => null;
+
+    private void InitializeLBS(UnityEditor.PackageManager.PackageInfo packageInfo)
+    {
+        Debug.Log("LEVEL BUILDING SIDEKICK");
 
         LBSSettings.assetName = "LBSUserSettings";
         LBSAssetsStorage.assetName = "Storage";
@@ -36,29 +59,19 @@ public class ISI_Lab_PackageManager : IPackageManagerExtension
 
         CreateFolderIfItDoesntExist(userFolderPath, userFolder);
 
-        foreach(string subfolder in new string[] { "Bundles", "Tags", "Meshes", "Settings", "Cache"})
+        foreach (string subfolder in new string[] { "Bundles", "Tags", "Meshes", "Settings", "Cache" })
         {
             CreateFolderIfItDoesntExist(userFolderFullPath, subfolder);
         }
 
-        if(AssetDatabase.FindAssets("LBSUserSettings", new string[] { userFolderFullPath + "/Settings" }).Length == 0)
+        if (AssetDatabase.FindAssets("LBSUserSettings", new string[] { userFolderFullPath + "/Settings" }).Length == 0)
         {
             AssetDatabase.CopyAsset(AssetDatabase.GUIDToAssetPath(defaultSettingsGUID), userFolderFullPath + "/Settings/LBSUserSettings.asset");
         }
-        if(AssetDatabase.FindAssets("Storage", new string[] {userFolderFullPath + "/Cache"}).Length == 0)
+        if (AssetDatabase.FindAssets("Storage", new string[] { userFolderFullPath + "/Cache" }).Length == 0)
         {
             AssetDatabase.CopyAsset(AssetDatabase.GUIDToAssetPath(defaultStorageGUID), userFolderFullPath + "/Cache/Storage.asset");
         }
-    }
-
-    public void OnPackageRemoved(UnityEditor.PackageManager.PackageInfo packageInfo)
-    {
-        
-    }
-
-    public void OnPackageSelectionChange(UnityEditor.PackageManager.PackageInfo packageInfo)
-    {
-        
     }
 
     private void CreateFolderIfItDoesntExist(string parent, string name)
