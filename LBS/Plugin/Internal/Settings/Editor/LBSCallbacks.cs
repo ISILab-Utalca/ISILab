24df2d202f992328bdcc145ff306dd3d4b4d933d
SebastianFranMartin <65563860+SebastianFranMartin@users.noreply.github.com>
2025-11-06 14:21:47 -0300

    Moved package initialization to LBSCallbacks

--- LBS/Plugin/Internal/Settings/Editor/LBSCallbacks.cs
+++ LBS/Plugin/Internal/Settings/Editor/LBSCallbacks.cs
@@ -13,6 +13,10 @@ namespace ISILab.LBS.Internal.Editor
     public class LBSCallbacks
     {
         private static BackUp localBackUp;
+
+        const string defaultSettingsGUID = "29abd09f3cff7644da7097258d0ae978";
+        const string defaultStorageGUID = "5dacd13b749bccf469893489a5d0f94b";
+
         static LBSCallbacks()
         {
             var onStart = SessionState.GetBool("start", true);
@@ -25,6 +29,13 @@ namespace ISILab.LBS.Internal.Editor
 
             AssemblyReloadEvents.afterAssemblyReload += OnAfterReloadScript;
             AssemblyReloadEvents.beforeAssemblyReload += OnBeforeReloadScript;
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var packageInfo = UnityEditor.PackageManager.PackageInfo.FindForAssembly(assembly);
+            if (packageInfo is not null && packageInfo.name.Equals("com.isilab.lbs"))
+            {
+                AssemblyReloadEvents.afterAssemblyReload += InitializeLBSPackage;
+            }
         }
 
         /// <summary>
@@ -146,6 +157,56 @@ namespace ISILab.LBS.Internal.Editor
                 bundle.Reload();
             }
         }
+        public static void InitializeLBSPackage()
+        {
+            Debug.Log("LEVEL BUILDING SIDEKICK");
 
+            // Crear carpetas de usuario LBS
+            string userFolderFullPath = "Assets/LBSUserContent";
+            List<string> pathFolders = new List<string>(userFolderFullPath.Split('/'));
+            string userFolder = pathFolders[pathFolders.Count - 1];
+            pathFolders.Remove(userFolder);
+            string userFolderPath = string.Join('/', pathFolders);
+            string resourcesFolderPath = userFolderFullPath + "/Resources";
+
+            CreateFolderIfItDoesntExist(userFolderPath, userFolder);
+            CreateFolderIfItDoesntExist(userFolderFullPath, "Resources");
+
+            foreach (string subfolder in new string[] { "Bundles", "Tags", "Meshes" })
+            {
+                CreateFolderIfItDoesntExist(userFolderFullPath, subfolder);
+            }
+            foreach (string subFolder in new string[] { "Settings", "Cache" })
+            {
+                CreateFolderIfItDoesntExist(resourcesFolderPath, subFolder);
+            }
+
+            if (AssetDatabase.FindAssets("LBSUserSettings", new string[] { resourcesFolderPath + "/Settings" }).Length == 0)
+            {
+                AssetDatabase.CopyAsset(AssetDatabase.GUIDToAssetPath(defaultSettingsGUID), resourcesFolderPath + "/Settings/LBSUserSettings.asset");
+            }
+            if (AssetDatabase.FindAssets("Storage", new string[] { resourcesFolderPath + "/Cache" }).Length == 0)
+            {
+                AssetDatabase.CopyAsset(AssetDatabase.GUIDToAssetPath(defaultStorageGUID), resourcesFolderPath + "/Cache/Storage.asset");
+            }
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            LBSSettings.assetName = "LBSUserSettings";
+            LBSSettings.ResetInstance();
+            LBSSettings.Instance.ReplacePaths();
+
+            LBSAssetsStorage.assetName = "Storage";
+            LBSAssetsStorage.folderName = "Cache";
+            LBSAssetsStorage.ResetInstance();
+        }
+        private static void CreateFolderIfItDoesntExist(string parent, string name)
+        {
+            if (!AssetDatabase.IsValidFolder(parent + "/" + name))
+            {
+                AssetDatabase.CreateFolder(parent, name);
+            }
+        }
     }
 }
