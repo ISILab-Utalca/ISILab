57d83b81522bedfc2103faf991121576b70f6a5c
RosenkreuzAlice <zakuphonic@gmail.com>
2025-12-03 18:02:04 -0300

    Grid progress

--- LBS/Plugin/VisualElements/Editor/Windows/BundleCharacteristics/TerrainConnectionGrid/AssetGridEditorWindow.cs
+++ LBS/Plugin/VisualElements/Editor/Windows/BundleCharacteristics/TerrainConnectionGrid/AssetGridEditorWindow.cs
@@ -2,6 +2,7 @@ using ISILab.Commons.Utility.Editor;
 using ISILab.LBS.Characteristics;
 using ISILab.LBS.Plugin.Components.Bundles;
 using LBS.Bundles;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -12,6 +13,21 @@ public class AssetGridEditorWindow : VisualElement
     VisualElement gridContainer;
     #endregion
 
+    #region VISUAL ELEMENTS
+    VisualElement thumbnail;
+    #endregion
+
+    #region SQUARE PREVIEW ELEMENTS
+
+    //Sourced from BundleDirectionEditorWindow
+    private Texture2D renderTexture;
+    private GameObject previewPrefab;
+    private PreviewRenderUtility prevRenderUtil;
+
+    private float fovScale;
+
+    #endregion
+
     #region PROPERTIES
     public AssetConnectionGrid AssetGrid => assetGrid;
     public VisualElement GridContainer => gridContainer;
@@ -19,7 +35,6 @@ public class AssetGridEditorWindow : VisualElement
     #endregion
 
     #region EVENTS
-
     #endregion
 
     #region CONSTRUCTOR
@@ -31,7 +46,26 @@ public class AssetGridEditorWindow : VisualElement
         visualTree.CloneTree(this);
 
         gridContainer = this.Q<VisualElement>("GridContainer");
-        SetGrid();
+
+        //Setting preview...
+        //Code is sourced from BundleDirectionEditorWindow. Let's see how much it can be translated from it
+        thumbnail = this.Q<VisualElement>("Thumbnail");
+        renderTexture = new Texture2D(512, 512, TextureFormat.RGBA32, false);
+        thumbnail.style.backgroundImage = new StyleBackground(renderTexture);
+
+        prevRenderUtil = new PreviewRenderUtility();
+        prevRenderUtil.cameraFieldOfView = 30f;
+
+        //Use AssetReference.obj to refer to the prefab
+        var _prefab = AssetReference.obj;
+        if (_prefab != null)
+        {
+            previewPrefab = prevRenderUtil.InstantiatePrefabInScene(_prefab);
+            previewPrefab.transform.position = Vector3.zero;
+        }
+        EditorApplication.delayCall += StepPreview;
+
+        //SetGrid();
     }
     #endregion
 
@@ -60,11 +94,34 @@ public class AssetGridEditorWindow : VisualElement
             }
         }
     }
+    
     #endregion
 
     public void UseToolOnTile(AssetGridTile tile)
     {
         Debug.Log("using tile nº"+tile.GridPosition+", VALUE: "+tile.ColorValue);
-        Debug.Log("terrain flag value: " + assetGrid.terrainFlag[tile.ColorValue]);
+        Debug.Log("terrain flag value: " + assetGrid.TerrainFlag[tile.ColorValue]);
+    }
+
+    private void StepPreview()
+    {
+        prevRenderUtil.BeginStaticPreview(new Rect(0, 0, 512, 512));
+
+        prevRenderUtil.camera.transform.position = new Vector3(0, 10, 0);
+        prevRenderUtil.camera.transform.rotation = Quaternion.Euler(90, 0, 0);
+
+        prevRenderUtil.camera.orthographic = true;
+
+        prevRenderUtil.camera.orthographicSize = fovScale;
+        prevRenderUtil.camera.nearClipPlane = 0.1f;
+        prevRenderUtil.camera.farClipPlane = 100f;
+
+        prevRenderUtil.lights[0].intensity = 1f;
+        prevRenderUtil.lights[0].transform.rotation = Quaternion.Euler(50f, 50f, 0);
+
+        prevRenderUtil.camera.Render();
+
+        renderTexture = prevRenderUtil.EndStaticPreview();
+        thumbnail.style.backgroundImage = new StyleBackground(renderTexture);
     }
 }
