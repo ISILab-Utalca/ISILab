813b4d9670c6ba9985fd6b8994365506b33eb79f
Nicolas Acevedo <roth81ly@gmail.com>
2025-12-15 13:35:35 -0300

    path os plus folder rename

--- LBS/Plugin/Modules/Simulation/PathOSPlus/OGVis/Scripts/OGLogVisualizer.cs
+++ LBS/Plugin/Modules/Simulation/PathOSPlus/OGVis/Scripts/OGLogVisualizer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using ISILab.LBS.Plugin.Modules.Simulation.PathOSPlus.OGVis.Scripts;
 using UnityEngine;
 using OGVis;
 
