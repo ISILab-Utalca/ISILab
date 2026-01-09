4f04fe8c0a7fe9d318005b5dd8abccdce9f7437e
Nicolás Acevedo <45216823+rothbilly@users.noreply.github.com>
2026-01-09 12:42:56 -0300

    tool button progress

--- LBS/Plugin/UI/Editor/CustomComponents/LBSCustomLabel.cs
+++ LBS/Plugin/UI/Editor/CustomComponents/LBSCustomLabel.cs
@@ -1,6 +1,40 @@
+
+
+using UnityEngine.UIElements;
+using UnityEditor;
 using UnityEngine;
 
-public class LBSCustomLabel
+namespace ISILab.LBS.CustomComponents
 {
     
+    [UxmlElement]
+    public partial class LBSCustomLabel: Label
+    {
+        
+        private Color hintColor;
+        
+        [UxmlAttribute]
+        public Color HintColor {
+            get { return hintColor; }
+            set
+            {
+                this.hintColor = value;
+            } 
+        }
+        
+        
+        
+        public LBSCustomLabel() : base()
+        {
+            RemoveFromClassList("");
+            AddToClassList("lbs-label");
+        }
+
+        public LBSCustomLabel(string _text = "") : this()
+        {
+            this.text = _text;
+        }
+        
+        
+    }
 }
