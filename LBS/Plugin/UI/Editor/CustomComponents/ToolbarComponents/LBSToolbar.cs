694ab9b6fc2e7f71c863fe5f68393bad463b455a
Nicolás Acevedo <45216823+rothbilly@users.noreply.github.com>
2026-01-12 12:02:19 -0300

    Added Toolbar accent color param

--- LBS/Plugin/UI/Editor/CustomComponents/ToolbarComponents/LBSToolbar.cs
+++ LBS/Plugin/UI/Editor/CustomComponents/ToolbarComponents/LBSToolbar.cs
@@ -9,12 +9,41 @@ namespace ISILab.LBS.CustomComponents
     [UxmlElement]
     public partial class LBSToolbar: Toolbar
     {
+        public enum AccentColor { DEFAULT, DARKEST }
+
+        private AccentColor toolbarAccentColor = AccentColor.DEFAULT;
+
+        [UxmlAttribute]
+        public AccentColor ToolbarAccentColor
+        {
+            get =>toolbarAccentColor;
+            set
+            {
+                toolbarAccentColor = value;
+                switch (value)
+                {
+                    case AccentColor.DEFAULT:
+                    {
+                        AddToClassList("prop-default-bg");
+                        RemoveFromClassList("prop-darkest-bg");
+                        break;   
+                    }
+                    case AccentColor.DARKEST:
+                    {
+                        AddToClassList("prop-darkest-bg");
+                        RemoveFromClassList("prop-default-bg");
+                        break;
+                    }
+                }
+            }
+        }
         
         readonly string _lbsClassName = "lbs-toolbar";
         public LBSToolbar() : base()
         {
             RemoveFromClassList(ussClassName);
             AddToClassList(_lbsClassName);
+            ToolbarAccentColor = toolbarAccentColor;
         }
     }
 }
