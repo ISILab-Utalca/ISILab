39f635fc5f7491d0e7dc547151800bd88bca7ec1
KevinATJ <kevintjara@gmail.com>
2026-01-14 11:03:24 -0300

    NewTest&NewMaps2
    - 8 new test for WFCBenchmark
    - new maps for WFCBenchmark

--- LBS/Tests/HillClimbBenchmarkReport.cs
+++ LBS/Tests/HillClimbBenchmarkReport.cs
@@ -13,8 +13,13 @@ namespace ISILab.LBS.Tests
     {
         LBSLevelData levelData ;
         HillClimbingAssistant HCassistant;
-        
-        
+        const string Map25Rooms = "4da6ebb0aca35d64b88743e3dadf267d";
+        const string Map16Rooms = "427ab32f305e74e41bad23bf41f74b05";
+        const string Map16Rooms_Simpler = "69981e3f2a94f1849a8008ba46c915cf";
+        const string Map9Rooms = "5751d55fd72bb1945b789e0ff542c4f5";
+        const string Map5Rooms = "d68863ab6554de747be765e86aa7ee9d";
+
+
         [Test, Performance]
         [Timeout(3600000)]
         public void MeasureHillClimbing_25_Rooms()
@@ -26,7 +31,7 @@ namespace ISILab.LBS.Tests
                 .WarmupCount(0)
                 .MeasurementCount(10)
                 .IterationsPerMeasurement(1)
-                .SetUp(() => SetupHillClimbTest("f97f322d306c1af43a8eaff23a17d2bd"))
+                .SetUp(() => SetupHillClimbTest(Map25Rooms))
                 .CleanUp(CleanUpHillClimbTest)
                 .Run();
         }
@@ -43,7 +48,7 @@ namespace ISILab.LBS.Tests
                 .WarmupCount(0)
                 .MeasurementCount(10)
                 .IterationsPerMeasurement(1)
-                .SetUp(() => SetupHillClimbTest("4f173a341c99d1a4b83b3a79c90da8e3"))
+                .SetUp(() => SetupHillClimbTest(Map16Rooms))
                 .CleanUp(CleanUpHillClimbTest)
                 .Run();
         }
@@ -60,7 +65,7 @@ namespace ISILab.LBS.Tests
                 .WarmupCount(0)
                 .MeasurementCount(10)
                 .IterationsPerMeasurement(1)
-                .SetUp(() => SetupHillClimbTest("d9637e558e0b60149983dd9589d35fd5"))
+                .SetUp(() => SetupHillClimbTest(Map16Rooms_Simpler))
                 .CleanUp(CleanUpHillClimbTest)
                 .Run();
         }
@@ -75,7 +80,7 @@ namespace ISILab.LBS.Tests
                 .WarmupCount(0)
                 .MeasurementCount(10)
                 .IterationsPerMeasurement(1)
-                .SetUp(() => SetupHillClimbTest("804037966f1f95945b7e07a51821e4fa"))
+                .SetUp(() => SetupHillClimbTest(Map9Rooms))
                 .CleanUp(CleanUpHillClimbTest)
                 .Run();
         }
@@ -90,7 +95,7 @@ namespace ISILab.LBS.Tests
                 .WarmupCount(0)
                 .MeasurementCount(10)
                 .IterationsPerMeasurement(1)
-                .SetUp(() => SetupHillClimbTest("a9427fc4639829544bb55127cacf1b0f"))
+                .SetUp(() => SetupHillClimbTest(Map5Rooms))
                 .CleanUp(CleanUpHillClimbTest)
                 .Run();
         }
--- LBS/Tests/WFCBenchmarkReport.cs
+++ LBS/Tests/WFCBenchmarkReport.cs
@@ -13,8 +13,18 @@ namespace ISILab.LBS.Tests
         LBSLevelData levelData ;
         AssistantWFC WFCassistant;
 
+        const string Edge_5x5_Map = "1e5437b06539cbc4bb2d78f0289eaa41";
+        const string Edge_10x10_Map = "82609bf5ea1bcc7419de21d82445fea5";
+        const string Edge_20x20_Map = "11800f84c2b82604f9a9e3f09d1a7fe3";
+        const string Edge_40x40_Map = "301ab417ce8d3da48986747b725c267a";
+        const string Vertex_5x5_Map = "6f1c8b94e4c5f2e4aba756e2d46dbb61";
+        const string Vertex_10x10_Map = "41185b8b66e1de3499b080546b6eb729";
+        const string Vertex_20x20_Map = "f44e1759c8bde004783ae5bbb9942e76";
+        const string Vertex_40x40_Map = "a75be50c10d15e14f934906c53ba56e1";
+
+
         [Test, Performance]
-        public void TestMap_5x5()
+        public void TestMap_5x5_Edge()
         {
             SampleGroup sg_01 = new SampleGroup("Run Time", SampleUnit.Millisecond); 
             SampleGroup sg_02 = new SampleGroup("GC Call", SampleUnit.Undefined); 
@@ -26,14 +36,14 @@ namespace ISILab.LBS.Tests
                 .WarmupCount(1)
                 .MeasurementCount(100)
                 .IterationsPerMeasurement(1)
-                .SetUp(() => SetupWFCTest("27e9e2296bf8411458f727b699a7c0fe"))
+                .SetUp(() => SetupWFCTest(Edge_5x5_Map))
                 .GC()
                 .CleanUp(CleanUpWFCTest)
                 .Run();
         }
         
         [Test, Performance]
-        public void TestMap_10x10()
+        public void TestMap_10x10_Edge()
         {
             Measure.Method(() =>
                 {
@@ -42,13 +52,13 @@ namespace ISILab.LBS.Tests
                 .WarmupCount(1)
                 .MeasurementCount(50)
                 .IterationsPerMeasurement(1)
-                .SetUp(() => SetupWFCTest("4da2c1388b315494ab46c235d9727bbe"))
+                .SetUp(() => SetupWFCTest(Edge_10x10_Map))
                 .CleanUp(CleanUpWFCTest)
                 .Run();
         }
         
         [Test, Performance]
-        public void TestMap_20x20()
+        public void TestMap_20x20_Edge()
         {
             Measure.Method(() =>
                 {
@@ -57,14 +67,14 @@ namespace ISILab.LBS.Tests
                 .WarmupCount(0)
                 .MeasurementCount(20)
                 .IterationsPerMeasurement(1)
-                .SetUp(() => SetupWFCTest("0e41f853d3bab334db4db772531db63c"))
+                .SetUp(() => SetupWFCTest(Edge_20x20_Map))
                 .CleanUp(CleanUpWFCTest)
                 .Run();
         }
         
         [Test, Performance]
         [Timeout(3600000)]
-        public void TestMap_40x40()
+        public void TestMap_40x40_Edge()
         {
             Measure.Method(() =>
                 {
@@ -73,16 +83,16 @@ namespace ISILab.LBS.Tests
                 .WarmupCount(0)
                 .MeasurementCount(10)
                 .IterationsPerMeasurement(1)
-                .SetUp(() => SetupWFCTest("01421564a101235499b4448d3801d494"))
+                .SetUp(() => SetupWFCTest(Edge_40x40_Map))
                 .CleanUp(CleanUpWFCTest)
                 .Run();
         }
         
         
         [Test, Performance]
-        public void TestMap_5x5_SameMap()
+        public void TestMap_5x5_SameMap_Edge()
         {
-            SetupWFCTest("27e9e2296bf8411458f727b699a7c0fe");
+            SetupWFCTest(Edge_5x5_Map);
             Measure.Method(() =>
                 {
                     Assert.AreEqual(true, WFCassistant.ExecuteTest(true));
@@ -97,9 +107,9 @@ namespace ISILab.LBS.Tests
         }
         
         [Test, Performance]
-        public void TestMap_10x10_SameMap()
+        public void TestMap_10x10_SameMap_Edge()
         {
-            SetupWFCTest("4da2c1388b315494ab46c235d9727bbe");
+            SetupWFCTest(Edge_10x10_Map);
             Measure.Method(() =>
                 {
                     Assert.AreEqual(true, WFCassistant.ExecuteTest(true));
@@ -113,9 +123,9 @@ namespace ISILab.LBS.Tests
         }
         
         [Test, Performance]
-        public void TestMap_20x20_SameMap()
+        public void TestMap_20x20_SameMap_Edge()
         {
-            SetupWFCTest("0e41f853d3bab334db4db772531db63c");
+            SetupWFCTest(Edge_20x20_Map);
             Measure.Method(() =>
                 {
                     Assert.AreEqual(true, WFCassistant.ExecuteTest(true));
@@ -130,9 +140,9 @@ namespace ISILab.LBS.Tests
         
         [Test, Performance]
         [Timeout(600000)]
-        public void TestMap_40x40_SameMap()
+        public void TestMap_40x40_SameMap_Edge()
         {
-            SetupWFCTest("01421564a101235499b4448d3801d494");
+            SetupWFCTest(Edge_40x40_Map);
             Measure.Method(() =>
                 {
                     Assert.AreEqual(true, WFCassistant.ExecuteTest(true));
@@ -154,6 +164,7 @@ namespace ISILab.LBS.Tests
             Assert.IsNotNull(fistLayer);
             WFCassistant = fistLayer.GetAssistant<AssistantWFC>("");
             Assert.IsNotNull(WFCassistant);
+            WFCassistant.SafeMode = true;
             fistLayer.Reload();
         }
         
@@ -167,6 +178,138 @@ namespace ISILab.LBS.Tests
                 WFCassistant = null;
                 levelData = null;
             }
+        }
+
+        [Test, Performance]
+        public void TestMap_5x5_Vertex()
+        {
+            SampleGroup sg_01 = new SampleGroup("Run Time", SampleUnit.Millisecond);
+            SampleGroup sg_02 = new SampleGroup("GC Call", SampleUnit.Undefined);
+
+            Measure.Method(() =>
+            {
+                Assert.AreEqual(true, WFCassistant.ExecuteTest(false));
+            })
+                .WarmupCount(1)
+                .MeasurementCount(100)
+                .IterationsPerMeasurement(1)
+                .SetUp(() => SetupWFCTest(Vertex_5x5_Map))
+                .GC()
+                .CleanUp(CleanUpWFCTest)
+                .Run();
+        }
+
+        [Test, Performance]
+        public void TestMap_10x10_Vertex()
+        {
+            Measure.Method(() =>
+            {
+                Assert.AreEqual(true, WFCassistant.ExecuteTest(false));
+            })
+                .WarmupCount(1)
+                .MeasurementCount(50)
+                .IterationsPerMeasurement(1)
+                .SetUp(() => SetupWFCTest(Vertex_10x10_Map))
+                .CleanUp(CleanUpWFCTest)
+                .Run();
+        }
+
+        [Test, Performance]
+        public void TestMap_20x20_Vertex()
+        {
+            Measure.Method(() =>
+            {
+                Assert.AreEqual(true, WFCassistant.ExecuteTest(false));
+            })
+                .WarmupCount(0)
+                .MeasurementCount(20)
+                .IterationsPerMeasurement(1)
+                .SetUp(() => SetupWFCTest(Vertex_10x10_Map))
+                .CleanUp(CleanUpWFCTest)
+                .Run();
+        }
+
+        [Test, Performance]
+        [Timeout(3600000)]
+        public void TestMap_40x40_Vertex()
+        {
+            Measure.Method(() =>
+            {
+                Assert.AreEqual(true, WFCassistant.ExecuteTest(false));
+            })
+                .WarmupCount(0)
+                .MeasurementCount(10)
+                .IterationsPerMeasurement(1)
+                .SetUp(() => SetupWFCTest(Vertex_40x40_Map))
+                .CleanUp(CleanUpWFCTest)
+                .Run();
+        }
+
+
+        [Test, Performance]
+        public void TestMap_5x5_SameMap_Vertex()
+        {
+            SetupWFCTest(Vertex_5x5_Map);
+            Measure.Method(() =>
+            {
+                Assert.AreEqual(true, WFCassistant.ExecuteTest(true));
+            })
+                .WarmupCount(1)
+                .MeasurementCount(100)
+                .IterationsPerMeasurement(1)
+                .GC()
+                .Run();
+
+            CleanUpWFCTest();
+        }
+
+        [Test, Performance]
+        public void TestMap_10x10_SameMap_Vertex()
+        {
+            SetupWFCTest(Vertex_10x10_Map);
+            Measure.Method(() =>
+            {
+                Assert.AreEqual(true, WFCassistant.ExecuteTest(true));
+            })
+                .WarmupCount(1)
+                .MeasurementCount(50)
+                .IterationsPerMeasurement(1)
+                .Run();
+
+            CleanUpWFCTest();
+        }
+
+        [Test, Performance]
+        public void TestMap_20x20_SameMap_Vertex()
+        {
+            SetupWFCTest(Vertex_20x20_Map);
+            Measure.Method(() =>
+            {
+                Assert.AreEqual(true, WFCassistant.ExecuteTest(true));
+            })
+                .WarmupCount(0)
+                .MeasurementCount(20)
+                .IterationsPerMeasurement(1)
+                .Run();
+
+            CleanUpWFCTest();
+        }
+
+        [Test, Performance]
+        [Timeout(600000)]
+        public void TestMap_40x40_SameMap_Vertex()
+        {
+            SetupWFCTest(Vertex_40x40_Map);
+            Measure.Method(() =>
+            {
+                Assert.AreEqual(true, WFCassistant.ExecuteTest(true));
+            })
+                .WarmupCount(0)
+                .MeasurementCount(10)
+                .IterationsPerMeasurement(1)
+                .Run();
+
+            CleanUpWFCTest();
         }
     }
 }
