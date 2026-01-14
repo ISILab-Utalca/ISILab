4ffe2b671fc0b1c9fd82e710a62c20314c12729e
KevinATJ <kevintjara@gmail.com>
2026-01-14 17:46:02 -0300

    NewLoadMaps
    - new maps for LoadLbsFile

--- LBS/Tests/LoadLBSFilesBenchmarkReport.cs
+++ LBS/Tests/LoadLBSFilesBenchmarkReport.cs
@@ -7,12 +7,16 @@ namespace ISILab.LBS.Tests
     [TestFixture]
     public class LoadLBSFilesBenchmarkReport
     {
+        const string LargeMapGUID = "694c2e9d3afd40e41b585c191eeaf4ab";
+        const string MediumMapGUID = "1599cf2b434e6724fb173af02f7fb1b1";
+        const string SmallMapGUID = "346bf6d52b5ef004e9453887fb4e0e91";
+
         [Test, Performance]
         public void LoadLargeMap()
         {
             Measure.Method(() =>
                 {
-                    LBSLevelData loaded = JSONDataManager.LoadDataByGUID<LBSLevelData>("01421564a101235499b4448d3801d494");
+                    LBSLevelData loaded = JSONDataManager.LoadDataByGUID<LBSLevelData>(LargeMapGUID);
                     Assert.IsNotNull(loaded);
                 
                 })
@@ -20,13 +24,27 @@ namespace ISILab.LBS.Tests
                 .MeasurementCount(20)
                 .Run();
         }
-        
+
+        [Test, Performance]
+        public void LoadMediumMap()
+        {
+            Measure.Method(() =>
+            {
+                LBSLevelData loaded = JSONDataManager.LoadDataByGUID<LBSLevelData>(MediumMapGUID);
+                Assert.IsNotNull(loaded);
+
+            })
+                .WarmupCount(3)
+                .MeasurementCount(20)
+                .Run();
+        }
+
         [Test, Performance]
         public void LoadSmallMap()
         {
             Measure.Method(() =>
                 {
-                    LBSLevelData loaded = JSONDataManager.LoadDataByGUID<LBSLevelData>("27e9e2296bf8411458f727b699a7c0fe");
+                    LBSLevelData loaded = JSONDataManager.LoadDataByGUID<LBSLevelData>(SmallMapGUID);
                     Assert.IsNotNull(loaded);
                     
                 })
