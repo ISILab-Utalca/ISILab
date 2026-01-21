92b162e3df1545af0dc00550d68fc3d0640af773
Nicolás Acevedo <45216823+rothbilly@users.noreply.github.com>
2026-01-21 12:37:30 -0300

    random seed slider

--- LBS/Plugin/UI/Editor/CustomComponents/ComplexComponents/LBSIntSeedField/LBSCustomRandomSeedField.cs
+++ LBS/Plugin/UI/Editor/CustomComponents/ComplexComponents/LBSIntSeedField/LBSCustomRandomSeedField.cs
@@ -1,8 +1,9 @@
 using System;
 using System.Collections.Generic;
-using UnityEngine.UI;
+using UnityEngine;
 using UnityEngine.UIElements;
 using Button = UnityEngine.UIElements.Button;
+using Random = UnityEngine.Random;
 
 namespace ISILab.LBS.Plugin.UI.Editor.CustomComponents.ComplexComponents
 {
@@ -22,24 +23,40 @@ namespace ISILab.LBS.Plugin.UI.Editor.CustomComponents.ComplexComponents
         };
 
         private Button m_diceButton;
+        private TextField m_inputField;
 
         public LBSCustomRandomSeedField()
         {
-            AddToClassList("lbs-ramdom-int-field");
+            AddToClassList("lbs-random-int-field");
             
             m_diceButton = new Button();
+            m_diceButton.AddToClassList("lbs-icon");
             this.Add(m_diceButton);
             m_diceButton.SendToBack();
             m_diceButton.style.backgroundImage = new StyleBackground(Macros.LBSAssetMacro.LoadAssetByGuid<VectorImage>(ICON_GUID[0]));
             m_diceButton.RegisterCallback<ClickEvent>(_evt => GenerateRandomSeed());
             //m_diceButton.BringToFront(); // The opposite to SendToBack
             this.highValue = Int32.MaxValue;
+
         }
 
 
         public void GenerateRandomSeed()
         {
-            
+            int seed = Random.Range(0, Int32.MaxValue);
+            value = seed;
+            RandomizeIcon();
+            this.MarkDirtyRepaint();
+        }
+
+        private void RandomizeIcon()
+        {
+            int selected = value % 6;
+            if (m_diceButton != null)
+            {
+                VectorImage newIcon  = Macros.LBSAssetMacro.LoadAssetByGuid<VectorImage>(ICON_GUID[selected]);
+                m_diceButton.style.backgroundImage = new StyleBackground(newIcon);
+            }
         }
     }
 }
