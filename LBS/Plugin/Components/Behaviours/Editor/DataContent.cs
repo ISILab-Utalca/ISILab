6a6b1f664b0d3f9f897ae82fdf50008560b9fd7d
Nicolás Acevedo <45216823+rothbilly@users.noreply.github.com>
2026-02-19 10:04:39 -0300

    change green style color

--- LBS/Plugin/Components/Behaviours/Editor/DataContent.cs
+++ LBS/Plugin/Components/Behaviours/Editor/DataContent.cs
@@ -15,7 +15,7 @@ namespace ISILab.LBS.VisualElements
 
         public DataContent(VisualElement content, string name)
         {
-            var visualTree = DirectoryTools.GetAssetByName<VisualTreeAsset>("DataContent");
+            VisualTreeAsset visualTree = DirectoryTools.GetAssetByName<VisualTreeAsset>("DataContent");
             visualTree.CloneTree(this);
 
             // Foldout
