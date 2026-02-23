5bc337c7122a0a3d0d2ef6d5dee2fabd36f92564
Nicolás Acevedo <45216823+rothbilly@users.noreply.github.com>
2026-02-23 12:21:05 -0300

    tags manager wimdow

--- LBS/Plugin/UI/Editor/Windows/TagsManager/TagManagerWindow.cs
+++ LBS/Plugin/UI/Editor/Windows/TagsManager/TagManagerWindow.cs
@@ -1,17 +1,42 @@
+using System;
+using ISILab.Commons.Utility.Editor;
 using ISILab.DevTools.Macros;
 using ISILab.LBS.Plugin.UI.Editor.Windows;
 using UnityEditor;
 using UnityEngine;
-
+using UnityEngine.UIElements;
 
 
 public class TagManagerWindow :ThemeableWindow
 {
+    
+    
+    public static TagManagerWindow Instance { get; private set; }
+    
+    
     [MenuItem("Window/ISILab/Tag Manager", priority = 2)]
     public static void ShowWindow()
     {
         TagManagerWindow window = GetWindow<TagManagerWindow>();
-        Texture icon = AssetMacro.LoadAssetByGuid<Texture>("6351057aa17189c44902075c0b9353fd");
+        Texture icon = AssetMacro.LoadAssetByGuid<Texture>("40d548834301ba14f96af3e1715add5f");
         window.titleContent = new GUIContent("Tag Manager", icon);
+    }
+
+    
+    //Singleton part
+    private void OnEnable()
+    {
+        Instance = this;
+    }
+
+    private void CreateGUI()
+    {
+        VisualTreeAsset visualTree = DirectoryTools.GetAssetByName<VisualTreeAsset>("TagManagerWindow");
+        visualTree.CloneTree(rootVisualElement);
+    }
+    
+    private void OnDisable()
+    {
+        Instance = null;
     }
 }
