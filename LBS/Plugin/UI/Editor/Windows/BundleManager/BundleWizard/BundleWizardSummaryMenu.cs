822a169080c922b4678af0ca6896a09b8694331f
Ignacio Reyes <files.ignacio98@gmail.com>
2026-02-25 13:12:54 -0300

    wizard bundle summary

--- LBS/Plugin/UI/Editor/Windows/BundleManager/BundleWizard/BundleWizardSummaryMenu.cs
+++ LBS/Plugin/UI/Editor/Windows/BundleManager/BundleWizard/BundleWizardSummaryMenu.cs
@@ -1,6 +1,7 @@
 using ISILab.LBS.CustomComponents;
 using ISILab.LBS.Plugin.Core.Settings;
 using ISILab.LBS.VisualElements;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -17,28 +18,62 @@ namespace ISILab.LBS.Plugin.UI.Editor.Windows.BundleManager.BundleWizard
         private string infoSummary;
         private string path;
 
-        private Label toChange;
         private WarningPanel warningPanel;
+        private LBSCustomLabelItem CLIPath, CLITBundles, CLINBundles, CLIABundles;
+        private LBSCustomButton goToPath;
 
         public void Init()
         {
-            toChange = this.Q<Label>("LabelInfo");
             warningPanel = this.Q<WarningPanel>("warningPanel");
+
+            CLIPath = this.Q<LBSCustomLabelItem>("LBSCustomLabelItemPath");
+            CLITBundles = this.Q<LBSCustomLabelItem>("LBSCustomLabelItemTBundles");
+            CLINBundles = this.Q<LBSCustomLabelItem>("LBSCustomLabelItemNBundles");
+            CLIABundles = this.Q<LBSCustomLabelItem>("LBSCustomLabelItemABundles");
+
+            goToPath = this.Q<LBSCustomButton>("goToPath");
+            if (goToPath != null)
+            {
+                goToPath.clicked += () => { OpenPathInProject(); };
+            }
 
             newBundles = Builder.newSubBundles.Count;
             assignedBundles = Builder.newAssignBundles.Count;
 
             SetInfoSummary();
-
-            toChange.text = infoSummary;
         }
 
         private void SetInfoSummary()
         {
             warningPanel.Text = "New main bundle \"" + Builder.bundleName + "\" succesfully created";
 
-            infoSummary = LBSSettings.Instance.paths.bundleFolderPath + "/" + Builder.bundleName + ".asset" 
-                          + " \n\n" + (assignedBundles).ToString()+ "\n\n" + newBundles.ToString()+ "\n\n" + (assignedBundles-newBundles).ToString();
+            CLIPath.TextR = LBSSettings.Instance.paths.bundleFolderPath + "/" + Builder.bundleName + ".asset";
+            CLITBundles.TextR = (assignedBundles).ToString();
+            CLINBundles.TextR = newBundles.ToString();
+            CLIABundles.TextR = (assignedBundles - newBundles).ToString();
+        }
+
+        private void OpenPathInProject()
+        {
+            //string folderPath = LBSSettings.Instance.paths.bundleFolderPath + "/" + Builder.bundleName + ".asset";
+            string folderPath = LBSSettings.Instance.paths.bundleFolderPath;
+
+            UnityEngine.Object obj = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(folderPath);
+
+            if (obj != null)
+            {
+                // Focus the Project window
+                EditorUtility.FocusProjectWindow();
+
+                // Select the object, which makes the project window jump to that folder
+                Selection.activeObject = obj;
+
+                // Optional: Ping the object to highlight it visually
+                EditorGUIUtility.PingObject(obj);
+
+                //OPEN FOLDER
+                AssetDatabase.OpenAsset(obj);
+            }
         }
 
         public void Revert()
