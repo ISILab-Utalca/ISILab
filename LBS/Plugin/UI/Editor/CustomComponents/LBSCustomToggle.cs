b0d60cb24fff6d48dbf80985362e6acb31561bd8
Ignacio Reyes <files.ignacio98@gmail.com>
2026-03-10 17:39:27 -0300

    Wizard Bundle Chars Custom Toggle Element and Style Fixed

--- LBS/Plugin/UI/Editor/CustomComponents/LBSCustomToggle.cs
+++ LBS/Plugin/UI/Editor/CustomComponents/LBSCustomToggle.cs
@@ -1,40 +1,32 @@
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 namespace ISILab.LBS.CustomComponents
 {
     [UxmlElement]
-    public partial class LBSCustomToggle : BaseField<bool>
+    public partial class LBSCustomToggle : UnityEngine.UIElements.Toggle
     {
         //string styles so they can be added later
 
-        private VisualElement m_Input;
-        private Toggle toggle;
+        //private VisualElement m_Input;
 
-        public LBSCustomToggle() : this("LBS Toggle") { }
-
-        public LBSCustomToggle(string label) : base(label, null)
+        public LBSCustomToggle() : base()
         {
-            RemoveFromClassList(ussClassName);
             //addToClassList para agregar los estilos necesarios
 
-            m_Input = this.Q(className: BaseField<bool>.inputUssClassName);
+            //m_Input = this.Q(className: BaseField<bool>.inputUssClassName);
+            //
+            RemoveFromClassList("unity-toggle");
+            AddToClassList("lbs-custom-toggle");
 
-
-
+            /*
+            RegisterCallback<ClickEvent>(_evt => OnClick(_evt));
+            RegisterCallback<KeyDownEvent>(evt => OnKeydownEvent(evt));
+            RegisterCallback<NavigationSubmitEvent>(evt => OnSubmit(evt));
+            */
         }
 
-
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
     }
 }
