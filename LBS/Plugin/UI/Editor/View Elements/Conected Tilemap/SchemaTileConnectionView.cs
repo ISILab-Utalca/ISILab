40a93f59f7f56c300c159ecb034b57fc7001730b
Rodrixam <85593181+Rodrixam@users.noreply.github.com>
2026-03-17 17:30:46 -0300

    SchemaTileConnectionView events (unused)

--- LBS/Plugin/UI/Editor/View Elements/Conected Tilemap/SchemaTileConnectionView.cs
+++ LBS/Plugin/UI/Editor/View Elements/Conected Tilemap/SchemaTileConnectionView.cs
@@ -1,10 +1,15 @@
 using ISILab.Commons.Utility.Editor;
 using ISILab.DevTools.Macros;
+using ISILab.LBS.Editor.Windows;
+using ISILab.LBS.Manipulators;
+using ISILab.LBS.Modules;
 using ISILab.LBS.Plugin.Components.Behaviours;
 using ISILab.LBS.Plugin.Components.Data.Tessellation.TileMap;
 using LBS.Components;
+using LBS.VisualElements;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace ISILab.LBS.VisualElements
@@ -14,21 +19,24 @@ namespace ISILab.LBS.VisualElements
         #region VIEW FIELDS
 
         private VisualElement Icon;
-        private LBSLayer lbsLayer;
+        private LBSLayer _ownerLayer;
         private LBSTile tile;
 
         #endregion
 
         #region STATIC FIELDS
 
+        public static SchemaTileConnectionView SelectedTile;
         private static VisualTreeAsset view;
 
         public static readonly Dictionary<string, string> ConnectionIconGuids =
            new()
            {
-            { SchemaBehaviour.Door,    "cd77d8067cf8b6b44ab23da9a62173c0" },
-            { SchemaBehaviour.Window,  "c0d00de1d82858c4b9d772a012caf67d" },
-            { SchemaBehaviour.LockedDoor,  "f79cf4ba5777aab4a884bca201ff0278" }
+               { SchemaBehaviour.Door,    "cd77d8067cf8b6b44ab23da9a62173c0" },
+               { SchemaBehaviour.Window,  "c0d00de1d82858c4b9d772a012caf67d" },
+               { SchemaBehaviour.LockedDoor,  "f79cf4ba5777aab4a884bca201ff0278" },
+               { SchemaBehaviour.StairsUp, "76bf813a38668ce439887addd209058c" },
+               { SchemaBehaviour.StairsDown, "76bf813a38668ce439887addd209058c" }
             //   ,{ SchemaBehaviour.BlockedDoor, "8e1818e3f49414e4997ecc63e331999f" },
            };
 
@@ -38,13 +46,18 @@ namespace ISILab.LBS.VisualElements
         #region FIELDS
         private string connectionType;
         private string connectionSide;
+        
+        private Color normalColor;
+        // Set outline width here NOT in uxml
+        private float normalBorder = 0f;
+        private float hoverIncrease = 0f;
         #endregion
 
         #region PROPERTIES
         public string Type { get => connectionType; }
         public string Direction { get => connectionSide; }
         public LBSTile Tile { get => tile; }
-        public LBSLayer Layer { get => lbsLayer; }
+        public LBSLayer Layer { get => _ownerLayer; }
         public static VectorImage GetIcon(string connection)
         {
             if (!IconCache.TryGetValue(connection, out VectorImage icon))
@@ -70,7 +83,7 @@ namespace ISILab.LBS.VisualElements
             Icon = this.Q<VisualElement>("Icon");
             List<string> connectionTypes = SchemaBehaviour.Connections;
 
-            this.lbsLayer = layer;
+            this._ownerLayer = layer;
             this.tile = tile;
             this.connectionType = connectionType;
             this.connectionSide = connectionSide;
@@ -84,6 +97,39 @@ namespace ISILab.LBS.VisualElements
 
             Icon.style.backgroundImage = new StyleBackground(connectionIcon);
 
-        } 
+            /*
+            RegisterCallback<PointerEnterEvent>(OnHover);
+            RegisterCallback<PointerLeaveEvent>(OnUnhover);
+            RegisterCallback<MouseDownEvent>(OnMouseDown);//*/
+        }
+
+        private void OnHover(PointerEnterEvent evt)
+        {
+            // Color change
+            normalColor = style.backgroundColor.value;
+            var _hoverColor = normalColor;
+            _hoverColor.a = 1f;
+            _hoverColor.r *= hoverIncrease;
+            _hoverColor.g *= hoverIncrease;
+            _hoverColor.b *= hoverIncrease;
+            style.backgroundColor = _hoverColor;
+        }
+
+        private void OnUnhover(PointerLeaveEvent evt)
+        {
+            style.backgroundColor = normalColor;
+        }
+        private void OnMouseDown(MouseDownEvent evt)
+        {
+            if (LBSMainWindow.Instance._selectedLayer != _ownerLayer) return;
+            if (ToolKit.Instance.GetActiveManipulator() is null) return;
+            if (ToolKit.Instance.GetActiveManipulator().GetType() == typeof(SelectManipulator))
+            {
+                //_tileBehaviour.SelectedTilemap = _tileBundleGroup;
+                LBSInspectorPanel.ActivateBehaviourTab();
+
+                SelectedTile = this;
+            }
+        }
     }
 }
