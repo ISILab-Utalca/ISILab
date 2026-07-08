0ad0ebfb7a45ca652f36e027dd0c9e588eb89ad1
Rodrixam <85593181+Rodrixam@users.noreply.github.com>
2026-07-08 19:33:40 -0400

    Add mod-selector UI and fade coroutines
    Introduce a ModSelector UI with four option buttons and adjust Icon sizing in Lbesin. Implement fade-in/out animations using Editor coroutines (ShowImage/HideImage) and manage coroutines per VisualElement with StartCoroutine/StopAllRoutines. Wire MouseEnter/MouseLeave and Reset callbacks to show/hide elements and set initial hidden state. Add required using directives for Unity.EditorCoroutines.Editor and UnityEngine.UI. Also update LBS.Editor.asmdef to include the new GUID for the added resource/asset.

--- LBS/Plugin/UI/Editor/Clippy/Lbesin.cs
+++ LBS/Plugin/UI/Editor/Clippy/Lbesin.cs
@@ -1,4 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.EditorCoroutines.Editor;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 [UxmlElement]
@@ -6,11 +10,14 @@ public partial class Lbesin : VisualElement
 {
     private readonly Vector2 offset = new Vector2(48,48);
 
+    private Dictionary<VisualElement, List<EditorCoroutine>> _activeCoroutines = new ();
     private bool dragging;
     private Vector2 dragOffset;
 
     VisualElement _draggable;
     VisualElement _reset;
+    VisualElement _modSelector;
+    VisualElement[] _modButtons;
 
     VisualElement Draggable 
     {
@@ -20,13 +27,29 @@ public partial class Lbesin : VisualElement
     {
         get => _reset ??= this.Q<VisualElement>("Reset");
     }
+    VisualElement ModSelector
+    {
+        get => _modSelector ??= Draggable.Q<VisualElement>("ModSelector");
+    }
+    VisualElement[] ModButtons
+    {
+        get => _modButtons ??= new[]
+        {
+            ModSelector.Q<VisualElement>("1"),
+            ModSelector.Q<VisualElement>("2"),
+            ModSelector.Q<VisualElement>("3"),
+            ModSelector.Q<VisualElement>("4")
+        };
+    }
 
     private Vector2 Position
     {
         get => new Vector2(Draggable.style.translate.value.x.value, Draggable.style.translate.value.y.value);
         set
         {
-            Draggable.style.translate = new Translate(new Length(value.x, LengthUnit.Pixel), new Length(value.y, LengthUnit.Pixel));
+            Draggable.style.translate = new Translate(
+                new Length(value.x, LengthUnit.Pixel), 
+                new Length(value.y, LengthUnit.Pixel));
         }
     }
 
@@ -36,15 +59,72 @@ public partial class Lbesin : VisualElement
         var visualTree = Resources.Load<VisualTreeAsset>("Lbesin");
         visualTree.CloneTree(this);
 
+        // Set callbacks
         Draggable.RegisterCallback<PointerDownEvent>(OnPointerDown);
         Draggable.RegisterCallback<PointerMoveEvent>(OnPointerMove);
         Draggable.RegisterCallback<PointerUpEvent>(OnPointerUp);
         Draggable.RegisterCallback<PointerCaptureOutEvent>(OnPointerCaptureOut);
 
-        Reset.RegisterCallback<ClickEvent>(evt => ResetPosition());
+        Draggable.RegisterCallback<MouseEnterEvent>(evt => 
+        {
+            StartCoroutine(ShowImage(ModSelector), ModSelector);
+            foreach (var button in ModButtons)
+            {
+                StartCoroutine(ShowImage(button), button);
+            }
+        });
+        Draggable.RegisterCallback<MouseLeaveEvent>(evt =>
+        {
+            StartCoroutine(HideImage(ModSelector), ModSelector);
+            foreach (var button in ModButtons)
+            {
+                StartCoroutine(HideImage(button), button);
+            }
+        });
+
+        Reset.RegisterCallback<ClickEvent>(evt => 
+        {
+            StartCoroutine(HideImage(Reset), Reset);
+            ResetPosition();
+        });
+
+        // Initial values
+        StartCoroutine(HideImage(Reset), Reset);
+        StartCoroutine(HideImage(ModSelector), ModSelector);
+        foreach (var button in ModButtons)
+        {
+            StartCoroutine(HideImage(button), button);
+        }
     }
 
+    #region COROUTINES
+    private IEnumerator ShowImage(VisualElement ve)
+    {
+        var c = ve.style.unityBackgroundImageTintColor.value;
+        var a = c.a;
+        do
+        {
+            a += 0.2f;
+            ve.style.unityBackgroundImageTintColor = new Color(c.r, c.g, c.b, a);
+            yield return new EditorWaitForSeconds(0.05f);
+        } while (a < 1);
+        ve.style.unityBackgroundImageTintColor = new Color(c.r, c.g, c.b, 1);
+    }
+    private IEnumerator HideImage(VisualElement ve)
+    {
+        var c = ve.style.unityBackgroundImageTintColor.value;
+        var a = c.a;
+        do
+        {
+            a -= 0.2f;
+            ve.style.unityBackgroundImageTintColor = new Color(c.r, c.g, c.b, a);
+            yield return new EditorWaitForSeconds(0.05f);
+        } while (a > 0);
+        ve.style.unityBackgroundImageTintColor = new Color(c.r, c.g, c.b, 0);
+    }
+    #endregion
 
+    #region POINTER EVENTS
     private void OnPointerDown(PointerDownEvent evt)
     {
         if (evt.button != 0) // Left mouse button
@@ -80,6 +160,28 @@ public partial class Lbesin : VisualElement
     {
         dragging = false;
     }
+    #endregion
+
+    private void StartCoroutine(IEnumerator routine, VisualElement owner)
+    {
+        if(!_activeCoroutines.ContainsKey(owner))
+        {
+            _activeCoroutines[owner] = new List<EditorCoroutine>();
+        }
+        StopAllRoutines(owner);
+        _activeCoroutines[owner].Add(EditorCoroutineUtility.StartCoroutine(routine, owner));
+    }
+    private void StopAllRoutines(VisualElement owner)
+    {
+        if (_activeCoroutines.ContainsKey(owner))
+        {
+            foreach (var coroutine in _activeCoroutines[owner])
+            {
+                EditorCoroutineUtility.StopCoroutine(coroutine);
+            }
+            _activeCoroutines[owner].Clear();
+        }
+    }
 
     private void KeepOnBounds()
     {
@@ -93,4 +195,5 @@ public partial class Lbesin : VisualElement
     {
         Position = Vector2.zero;
     }
+
 }
