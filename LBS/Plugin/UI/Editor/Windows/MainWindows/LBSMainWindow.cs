fabdac8d349700eb7fcf3c5f9262aeee78c42441
Ignacio Reyes <files.ignacio98@gmail.com>
2026-08-04 11:04:36 -0400

    LoadUITree created and moved from OnEnable

--- LBS/Plugin/UI/Editor/Windows/MainWindows/LBSMainWindow.cs
+++ LBS/Plugin/UI/Editor/Windows/MainWindows/LBSMainWindow.cs
@@ -212,7 +212,10 @@ namespace ISILab.LBS.Editor.Windows
                 LBS_AssetsPostProcessor.InitializeLBSPackage();
                 packageInitialized = true;
             }
+        }
 
+        private void LoadUITree()
+        {
             #region LOAD UI TREE
             //MainWindows UXML 
             VisualTreeAsset visualTree = DirectoryTools.GetAssetByName<VisualTreeAsset>("LBSMainWindow");
@@ -279,6 +282,7 @@ namespace ISILab.LBS.Editor.Windows
         protected override void CreateGUI()
         {
             //Debug.Log("[Main Window] - CreateGUI");
+            LoadUITree();
             Init();
             rootVisualElement.focusable = true;
             rootVisualElement.Focus();
