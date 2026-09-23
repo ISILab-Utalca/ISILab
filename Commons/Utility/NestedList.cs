f0a5af0f81e5897ecdd18490530cc5ea79402b52
Sebastián Martin <smartin20@alumnos.utalca.cl>
2026-09-23 17:34:45 -0300

    Update NestedList.cs
    Added to namespace.

--- Commons/Utility/NestedList.cs
+++ Commons/Utility/NestedList.cs
@@ -1,22 +1,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-/// <summary>
-/// A list container that can be serialized by Unity 
-/// </summary>
-/// <typeparam name="T"></typeparam>
-[System.Serializable]
-public class NestedList<T>
+namespace ISILab.Commons.Utility
 {
-    [SerializeField]
-    public List<T> list = new();
-
-    public T this[int index]
+    /// <summary>
+    /// A list container that can be serialized by Unity.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [System.Serializable]
+    public class NestedList<T>
     {
-        get { return list[index]; }
-        set { list[index] = value; }
-    }
+        /// <summary>
+        /// The real list stored.
+        /// </summary>
+        [SerializeField]
+        public List<T> list = new();
 
-    public void Add(T item) => list.Add(item);
+        /// <summary>
+        /// Indexer to access the list stored.
+        /// </summary>
+        /// <param name="index">Element in the list.</param>
+        /// <returns>The element of the list at the indicated position.</returns>
+        public T this[int index]
+        {
+            get { return list[index]; }
+            set { list[index] = value; }
+        }
+
+        /// <summary>
+        /// Adds an element to the list.
+        /// </summary>
+        /// <param name="item">Item to add.</param>
+        public void Add(T item) => list.Add(item);
+    }
 }
